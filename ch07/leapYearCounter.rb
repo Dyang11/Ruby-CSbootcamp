9090a556e2533177e34b6dcc374bdc3f888b8a7b
Daniel <34287179+Dyang11@users.noreply.github.com>
2018-01-01 03:44:02 -0800

    Update leapYearCounter.rb

--- ch07/leapYearCounter.rb
+++ ch07/leapYearCounter.rb
@@ -3,13 +3,21 @@ puts 'What is the starting year?'
 starting_year = gets.chomp
 puts 'And the ending year?'
 ending_year = gets.chomp
-puts 'The leap year(s) between ' + starting_year.to_s + ' and ' + ending_year.to_s + ' is(are):'
+puts 'The leap year(s) between ' + starting_year + ' and ' + ending_year + ' is(are):'
 year = starting_year
 number_leap_years = 0
 while year.to_i <= ending_year.to_i
-  if (year.to_i % 4) == 0
+  if year.to_i % 400 == 0
     puts year
-    number_leap_years = (number_leap_years.to_i + 1)
+    number_leap_years = number_leap_years.to_i + 1
+  else
+    if year.to_i % 4 == 0 && year.to_i % 100 != 0
+    puts year
+    number_leap_years = number_leap_years.to_i + 1
+    end
   end
-  year = (year.to_i + 1)
+  year = year.to_i + 1
+end
+if number_leap_years == 0
+  puts 'None'
 end
