06ee25a714de8c33e61c5874138589b2e276b38f
Daniel <34287179+Dyang11@users.noreply.github.com>
2018-01-01 03:45:07 -0800

    Update tableOfContents.rb

--- ch08/tableOfContents.rb
+++ ch08/tableOfContents.rb
@@ -1,6 +1,6 @@
 line_width = 15
-tocs = ['Table of Contents', 'Chapter 1:', 'Getting Started', 'page  1', 'Chapter 2:', 'Numbers', 'page  9', 'Chapter 3:', 'Letters', 'page 13']
-puts tocs[0].center(line_width * 4)
-puts tocs[1].ljust(line_width) + tocs[2].ljust(line_width) + tocs[3].rjust(line_width*2)
-puts tocs[4].ljust(line_width) + tocs[5].ljust(line_width) + tocs[6].rjust(line_width*2)
-puts tocs[7].ljust(line_width) + tocs[8].ljust(line_width) + tocs[9].rjust(line_width*2)
+tocs = [['Chapter 1:', 'Getting Started', 'page  1'], ['Chapter 2:', 'Numbers', 'page  9'], ['Chapter 3:', 'Letters', 'page 13']]
+puts 'Table Of Contents'.center(line_width * 4)
+tocs.each do |toc|
+puts toc[0].ljust(line_width) + toc[1].ljust(line_width) + toc[2].rjust(line_width * 2)
+end
